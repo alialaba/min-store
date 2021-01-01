a0ba1e98d085956fcf9c0968246714fed3594599
alialaba <alialaba079@gmail.com>
2021-01-01 12:21:28 -0800

    adding some features to the app

--- script/index.js
+++ script/index.js
@@ -1,4 +1,8 @@
 const signUp = document.getElementById('sign_up');
+const signIn = document.getElementById('sign_in');
+const suggest = document.getElementById('suggest-item');
+const table = document.getElementById('tableData');
+
 
 const BASE_URL = 'https://jsminnastore.herokuapp.com';
 
@@ -30,22 +34,22 @@ const signUpHandler = (e) => {
             body: JSON.stringify(data)
         })
         .then((res) => res.json())
-        .then(function(data) {
+        .then((data) => {
             // Handle response you get from the server
-            console.log(data)
-            const user = data.payload;
-            sessionStorage.setItem(user, JSON.stringify(user));
-            window.location.href = "/store/suggest.html"
+            // console.log(data)
+            if (data.success === true) {
+                const user = data.payload;
+                sessionStorage.setItem('user', JSON.stringify(user));
+                window.location.href = "signIn.html"
+            } else {
+                setError(data.message)
+            }
 
-        }).catch(function(error) {
-            console.log(error);
-        });
+        }).catch((err) => console.log(err.message));
 
-    // console.log(data)
+
 }
 
-
-const signIn = document.getElementById('sign_in');
 
 const signInHandler = (e) => {
 
@@ -58,28 +62,91 @@ const signInHandler = (e) => {
         email,
         password,
     }
+
+    fetch(`${BASE_URL}/auth/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+
+            },
+            body: JSON.stringify(data)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data);
+            if (data.success === true) {
+                const user = data.payload;
+                //sesesion storage is use to match the recieved datas
+                sessionStorage.setItem('user', JSON.stringify(user));
+                window.location.href = "suggest.html"
+            } else {
+                setError(data.message);
+            }
+        }).catch((err) => console.log(err.message))
+
+
+}
+
+const suggestHandler = (e) => {
+    e.preventDefault();
+    let itemName = suggest.elements[name = 'name'];
+    let itemDescribe = suggest.elements[name = 'describe'];
+    let itemCategory = suggest.elements[name = "Category"];
+    let itemReason = suggest.elements[name = "reason"];
+
+    const data = {
+        itemName,
+        itemDescribe,
+        itemCategory,
+        itemReason,
+    }
     try {
-        fetch(`${BASE_URL}/auth/login`, {
-                method: 'POST',
+        fetch(`${BASE_URL}/suggest`, {
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    // Authorization: "Bearer " + user.token,
+                    Authorization: "Bearer " + user.token,
                 },
-                body: JSON.stringify(data)
+                body: JSON.stringify(data),
             })
             .then((res) => res.json())
             .then((data) => {
-                // console.log(data);
                 const user = data.payload;
                 //sesesion storage is use to match the recieved datas
-                sessionStorage.setItem(user, JSON.stringify(user));
-                window.location.href = "/store/suggest.html"
-
+                sessionStorage.setItem('user', JSON.stringify(user));
+                window.location.href = "/store/suggested.html";
 
             }).catch((err) => console.log(err))
-
     } catch (err) {
         console.log(err)
     }
+
+
+}
+
+const fetchAllData = () => {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    fetch(`${BASE_URL}/suggested`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + user.token,
+            },
+            body: JSON.stringify(data),
+        }).then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        }).catch((err) => console.log(err.message));
+
+
+}
+
+
+//setting error
+const setError = (input, message) => {
+    let fieldInput = input.parentElement;
+    let small = fieldInput.querySelector('small');
+    small.innerText = message;
+    fieldInput.className = "field error";
 
 }
